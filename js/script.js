fc4ceae523d238eeb2955e4570678707edc25f34
William Barela <william.barela.dev@gmail.com>
2020-01-15 21:36:28 -0600

    add function to allow mutliple pages to be produced

--- js/script.js
+++ js/script.js
@@ -6,13 +6,17 @@
   var seminars = 'https://raw.githubusercontent.com/WilliamBarela/eeb-seminar/master/seminars/2020_spring.yml';
 
   /* fetch statement which uses the updateUISuccess and updateUIFailure functions to create the UI */
-  fetch(seminars).then(function (response) {
-    return response.text();
-  }).then(function (response) {
-    updateUISuccess(response);
-  }).catch(function () {
-    updateUIFailure();
-  });
+  buildSeminars(seminars);
+
+  function buildSeminars(seminar_list){
+    fetch(seminar_list).then(function (response) {
+      return response.text();
+    }).then(function (response) {
+      updateUISuccess(response);
+    }).catch(function () {
+      updateUIFailure();
+    });
+  }
 
   /* in case of fetch success, this function to creates the UI */
   function updateUISuccess(response) {
